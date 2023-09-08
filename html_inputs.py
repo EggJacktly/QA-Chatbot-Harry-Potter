6d2fb96e4a851f5ba9dceec1c0237db2ce0525c1
Jaikishan Khatri <jaikishan.kha3@gmail.com>
2023-09-08 13:03:57 +0200

    Public release version

--- html_inputs.py
+++ html_inputs.py
@@ -1,3 +1,5 @@
+from langchain import PromptTemplate
+
 bot_template = '''
 <div class="chat-message bot">
     <div class="message">{{MSG}}</div>
@@ -9,5 +11,21 @@ user_template = '''
     <div class="message">{{MSG}}</div>
 </div>
 '''
+
+prompt_template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Answer in the same language the question was asked.
+Use three sentences maximum and keep the answer as concise as possible.
+
+{context}
+
+Question: {question}
+Answer:"""
+
+
+CUSTOM_PROMPT = PromptTemplate(
+    template = prompt_template, 
+    input_variables = ["context", "question"]
+)
 
 
