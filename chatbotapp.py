594cec6022953b0fc8d1a56aba0ad44cc278ae69
Jaikishan Khatri <jaikishan.kha3@gmail.com>
2023-09-08 18:14:45 +0200

    Public release chatbotapp.py, working with English models.

--- chatbotapp.py
+++ chatbotapp.py
@@ -2,7 +2,6 @@ import os
 
 import streamlit as st
 import torch
-from dotenv import load_dotenv
 from langchain import FAISS, HuggingFaceHub, HuggingFacePipeline
 from langchain.chains import ConversationalRetrievalChain
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
@@ -11,7 +10,15 @@ from langchain.memory import ConversationBufferMemory
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-from html_inputs import user_template, bot_template
+from html_inputs import bot_template, user_template, css, CUSTOM_PROMPT
+import warnings
+
+warnings.filterwarnings("ignore")
+
+# import best model parameters
+with open('./model_comparison/best_model/best-model-parameters.txt', 'r') as f:
+    best_model = f.read()
+f.close()
 
 
 def get_raw_pdf(pdfs_path):
@@ -26,10 +33,11 @@ def get_raw_pdf(pdfs_path):
     return documents
 
 
-def get_document_chunks(documents):
+def get_document_chunks(documents, split_chunk_size, split_overlap):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,  # split_chunk_size
-        chunk_overlap=200,  # split_overlap
+        chunk_size=split_chunk_size,
+        chunk_overlap=split_overlap,
+        separators=["\n\n", "\n", "\t", " ", ""],
         length_function=len,
         add_start_index=True,
     )
@@ -39,9 +47,9 @@ def get_document_chunks(documents):
 
 def get_vectorstore(doc_chunks, language):
     if language == "en":
-        model_name = "BAAI/bge-large-en"
+        model_name = best_model.split('\n')[2]
     elif language == "es":
-        model_name = "intfloat/multilingual-e5-large"
+        model_name = best_model.split('\n')[2]
     else:
         raise ValueError("language must be 'en' or 'es'")
 
@@ -50,9 +58,9 @@ def get_vectorstore(doc_chunks, language):
         model_kwargs={"device": "cuda"}
     )
 
-    if os.path.isfile(f"data/vectorstore_{language}/index.faiss"):
+    if os.path.isfile(f"data/vectorstore-{language}/index.faiss"):
         vectorstore = FAISS.load_local(
-            f"data/vectorstore_{language}",
+            f"data/vectorstore-{language}",
             embeddings
         )
     else:
@@ -60,14 +68,13 @@ def get_vectorstore(doc_chunks, language):
             doc_chunks,
             embeddings
         )
-        vectorstore.save_local(f"data/vectorstore_{language}")
+        vectorstore.save_local(f"data/vectorstore-{language}")
     return vectorstore
 
 
 def create_model(model_repo):
     tokenizer = AutoTokenizer.from_pretrained(model_repo, use_fast=True)
-    # model = AutoModelForCausalLM.from_pretrained(
-    model = AutoModelForConditionalGeneration.from_pretrained(
+    model = AutoModelForCausalLM.from_pretrained(
         model_repo,
         load_in_4bit=True,
         device_map='auto',
@@ -78,13 +85,7 @@ def create_model(model_repo):
     return model, tokenizer
 
 
-def get_qa_chain(vectorstore, selected_model, temperature, max_length, top_p, repetition_penalty=1.15):
-    # llm = HuggingFaceHub(repo_id=selected_model,
-    #                      model_kwargs={"temperature": temperature,
-    #                                    "max_length": max_length,
-    #                                    "top_p": top_p}
-    #                      )
-
+def get_qa_chain(vectorstore, selected_model, temperature, max_length, top_p, repetition_penalty, k):
     model_, tokenizer = create_model(selected_model)
 
     pipe = pipeline(
@@ -106,7 +107,8 @@ def get_qa_chain(vectorstore, selected_model, temperature, max_length, top_p, re
     )
     qa_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
-        retriever=vectorstore.as_retriever(),
+        retriever=vectorstore.as_retriever(search_kwargs={"k": k, "search_type": "similarity"}),
+        combine_docs_chain_kwargs={"prompt": CUSTOM_PROMPT},
         memory=memory
     )
     return qa_chain
@@ -115,7 +117,6 @@ def get_qa_chain(vectorstore, selected_model, temperature, max_length, top_p, re
 def handle_prompts(user_question):
     response = st.session_state.conversation({'question': user_question})
     st.session_state.chat_history = response['chat_history']
-
     for i, message in enumerate(st.session_state.chat_history):
         if i % 2 == 0:
             st.write(user_template.replace(
@@ -126,14 +127,15 @@ def handle_prompts(user_question):
 
 
 def clear_chat_history():
-    st.experimental_rerun()
-    # st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
+    st.session_state.chat_history = None
+    st.session_state.conversation = None
 
 
 def main():
     # load_dotenv()
     st.set_page_config(page_title='Chat with a wizard from Hogwarts 🧙‍♂️', page_icon='🧙‍♂️', layout='centered',
                        initial_sidebar_state="auto")
+    st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
@@ -143,26 +145,26 @@ def main():
     with st.sidebar:
         st.subheader('Models and parameters')
         selected_language = st.sidebar.selectbox('Choose a language / Elige un idioma',
-                                                 ['English', 'Spanish'],
+                                                 ['English'],
                                                  key='selected_language')
         if selected_language == 'English':
             language = 'en'
             sending_msg = 'Sending an owl to the wizard...'
-            send_key = "Send"
+            send_key = "Send an owl"
             header_msg = 'Chat with a wizard from Hogwarts'
             default_msg = 'Ask a question to a wizard from Hogwarts and get an answer from the Harry Potter books'
 
-            model1 = 'google/flan-t5-xxl'
+            model1 = best_model.split('\n')[1]
             model2 = 'daryl149/llama-2-7b-chat-hf'
 
         elif selected_language == 'Spanish':
             sending_msg = "Enviando un búho al mago..."
             language = 'es'
-            send_key = "Enviar"
+            send_key = "Enviar un búho"
             header_msg = 'Chatea con un mago de Hogwarts'
             default_msg = 'Haz una pregunta a un mago de Hogwarts y obtén una respuesta de los libros de Harry Potter'
 
-            model1 = 'tiiuae/falcon-180B-chat'
+            model1 = best_model.split('\n')[1]
             model2 = 'bigscience/bloom'
 
         else:
@@ -171,9 +173,18 @@ def main():
         selected_model = st.sidebar.selectbox('Choose a LLM model', [model1, model2],
                                               key='selected_model')
 
-        temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
-        top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.95, step=0.01)
+        temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0,
+                                        value=float(best_model.split('\n')[4]), step=0.01)
+        top_p = st.sidebar.slider('top_p', min_value=0.5, max_value=1.0, value=float(best_model.split('\n')[5]),
+                                  step=0.01)
         max_length = st.sidebar.slider('max_length', min_value=512, max_value=4096, value=2048, step=512)
+        k = st.sidebar.slider('k', min_value=1, max_value=10, value=5, step=1)
+        repetition_penalty = st.sidebar.slider('repetition_penalty', min_value=1.0, max_value=2.0,
+                                               value=float(best_model.split('\n')[6]), step=0.01)
+        split_chunk_size = st.sidebar.slider('split_chunk_size', min_value=100, max_value=1000,
+                                             value=int(best_model.split('\n')[7]), step=100)
+        split_overlap = st.sidebar.slider('split_overlap', min_value=0, max_value=500,
+                                          value=int(best_model.split('\n')[8]), step=100)
 
         if st.button(send_key, key='send_button'):
             with st.spinner(sending_msg):
@@ -181,14 +192,14 @@ def main():
                 documents = get_raw_pdf(f"data/hp-books-{language}")
 
                 # splitting: split pdfs to chunks
-                doc_chunks = get_document_chunks(documents)
+                doc_chunks = get_document_chunks(documents, split_chunk_size, split_overlap)
 
                 # storage: create vectorstore to store chunks with embeddings
                 vectorstore = get_vectorstore(doc_chunks, language)
 
                 # chatbot: create chatbot with vectorstore
                 st.session_state.conversation = get_qa_chain(vectorstore, selected_model, temperature, max_length,
-                                                             top_p)
+                                                             top_p, repetition_penalty, k)
 
     st.header(header_msg + ' 🧙‍')
     user_question = st.text_input(default_msg + ' 📚')
@@ -196,11 +207,6 @@ def main():
     if user_question:
         with st.spinner(sending_msg):
             handle_prompts(user_question)
-
-    # # Display or clear chat messages
-    # for message in st.session_state.messages:
-    #     with st.chat_message(message["role"]):
-    #         st.write(message["content"])
 
     st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
--- html_inputs.py
+++ html_inputs.py
@@ -1,13 +1,46 @@
 from langchain import PromptTemplate
 
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
 bot_template = '''
 <div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://i.ibb.co/q9jZGwb/wizard.png">
+    </div>
     <div class="message">{{MSG}}</div>
 </div>
 '''
 
 user_template = '''
 <div class="chat-message user">
+    <div class="avatar">
+        <img src="https://i.ibb.co/DC4Lgdn/human.png">
+    </div>    
     <div class="message">{{MSG}}</div>
 </div>
 '''
@@ -25,7 +58,7 @@ Answer:"""
 
 CUSTOM_PROMPT = PromptTemplate(
     template = prompt_template, 
-    input_variables = ["context", "question"]
+    input_variables=["context", "question"]
 )
 
 
